fd821a9c2f669b27520e2a204391b89c24ea355b
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-04-13 16:17:27 +0400

    Which of the following are not valid variable names?

---
+++ src/l_DataTypes/Q_1.java
@@ -0,0 +1,12 @@
+package l_DataTypes;
+
+public class Q_1 {
+    public static void main(String[] args) {
+        // Which of the following are not valid variable names?
+        int _blue=4;
+        // int 2blue=4;
+        // int _=4;
+        int blue$=4;
+        int Blue=4;
+    }
+}
