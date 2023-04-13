ec18fee918ae9ebeffdc70c987278cee3755c239
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-04-13 16:18:09 +0400

    Which is equivalent to var q = 4.0f;?

---
+++ src/l_DataTypes/Q_3.java
@@ -0,0 +1,12 @@
+package l_DataTypes;
+
+public class Q_3 {
+    // Which is equivalent to var q = 4.0f;?
+    public static void main(String[] args) {
+        float q=4.0f;
+        Float q1=4.0f;
+        Double q2=4.0;
+        double q3=4.0f;
+        Object q4=4.0f;
+    }
+}
