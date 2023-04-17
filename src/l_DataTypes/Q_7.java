73b3a468af9c0005ddcd2452bb7ba0690e299516
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-04-17 12:24:07 +0400

    Which can fill in the blank? (Choose two.)

---
+++ src/l_DataTypes/Q_7.java
@@ -0,0 +1,22 @@
+package l_DataTypes;
+
+public class Q_7 {
+    /**
+     * Which can fill in the blank? (Choose two.)
+     * public void math() {
+     * _____ pi = 3.14;
+     * }
+     * A. byte
+     * B. double
+     * C. float
+     * D. short
+     * E. var
+     */
+    public static void main(String[] args) {
+        //  byte pi=3.14;
+        double pi1 = 3.14;
+        //  float pi2=3.14;
+        //  short pi3=3.14;
+        var pi4 = 3.14;
+    }
+}
